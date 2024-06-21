b7cb0bc564d5fff538263e8d26dc321729e9b300
SutuSebastian <sebiitv@gmail.com>
2024-06-21 16:44:23 +0300

    simplify tailwind.config; bump `flowbite-react` to latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,8 @@ import flowbite from 'flowbite-react/tailwind'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './inertia/**/*.{js,ts,jsx,tsx,vue}',
-    './resources/**/*.{js,ts,jsx,tsx,vue}',
-    './resources/**/*.edge',
+    './inertia/**/*.{js,ts,jsx,tsx}',
+    './resources/**/*.{edge,js,ts,jsx,tsx}',
     flowbite.content(),
   ],
   theme: {
